b2f6123d9285f055bbc17af6ce9aee38b61fb224
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-07 14:19:12 -0600

    Finish the string concatenation lesson

--- 01-variables-data-types/04-strings-concatenation/index.php
+++ 01-variables-data-types/04-strings-concatenation/index.php
@@ -1,3 +1,9 @@
+<?php
+  $firstName = 'John';
+  $lastName = 'Doe';
+  $fullName = $firstName . ' ' . $lastName;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,6 +23,13 @@
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6">
       <!-- Output -->
+      <?= 'Hello, my name is ' . $fullName . '<br />' ?>
+      <!-- Single quote concatonation is slightly faster -->
+      <?= "Hello, my name is $fullName <br/ >" ?> 
+      <?= 'Hello, my name is $fullName <br/ >' ?> 
+      <!-- This way doesn't flag an error, but the single quote interprets it as a string, and will not interpolate the variable! -->
+      <?= 'Hello, my name is \'John\'' ?>
+      <!-- The backslashes here will ignore the next character that comes up if it's significant to the system. In this case it's the single quote. So it's saved as a part of the string instead. -->
 
     </div>
   </div>
