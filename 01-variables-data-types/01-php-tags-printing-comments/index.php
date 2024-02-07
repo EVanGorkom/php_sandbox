2ec969da409a5d9bcad5cb03e5b56d1dfb006e9a
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-06 23:50:57 -0600

    Finish the first lesson on php tags

--- 01-variables-data-types/01-php-tags-printing-comments/index.php
+++ 01-variables-data-types/01-php-tags-printing-comments/index.php
@@ -1,5 +1,13 @@
 <?php
-echo 'Hello From PHP'; 
+// This is a single line comment
+/*
+This is a multi-line comment
+*/
+echo 'Hello From PHP';
+echo '<br/>';
+print 'Hello From Print';
+echo '<br/>';
+echo 'Value One, ', 'Value Two';
 ?>
 
 <!DOCTYPE html>
@@ -20,8 +28,8 @@ echo 'Hello From PHP';
     </header>
     <div class="container mx-auto p-4 mt-4">
         <div class="bg-white rounded-lg shadow-md p-6">
-            <h2 class="text-2xl font-semibold mb-4">Welcome To The Course</h2>
-            <p>In this course, you will learn the fundamentals of the PHP language</p>
+            <h2 class="text-2xl font-semibold mb-4"><?= 'Welcome To The Course' ?></h2>
+            <?php echo '<p>In this course, you will learn the fundamentals of the PHP language</p>'; ?>
         </div>
     </div>
 </body>
