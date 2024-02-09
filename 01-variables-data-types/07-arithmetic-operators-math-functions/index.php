08c5ca712191501f023531da0924466324a58e9a
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-09 14:05:20 -0600

    Finish the basic arithmatic oporators and built-ins lesson

--- 01-variables-data-types/07-arithmetic-operators-math-functions/index.php
+++ 01-variables-data-types/07-arithmetic-operators-math-functions/index.php
@@ -9,6 +9,56 @@
 | `/`      | Division       |
 | `%`      | Modulus        |
 */
+
+$num1 = 20;
+$num2 = 10;
+
+// Basic Math
+$output = "$num1 + $num2 = " . $num1 + $num2;
+$output = "$num1 - $num2 = " . $num1 - $num2;
+$output = "$num1 * $num2 = " . $num1 * $num2;
+$output = "$num1 / $num2 = " . $num1 / $num2;
+$output = "$num1 % $num2 = " . $num1 % $num2;
+
+// Assignment Operator
+$num3 = 10;
+// $num3 = $num3 + 20;
+// $num3 += 20; // this will equal 30
+// $num3 -= 20; // equals -10
+// $num3 *= 20; // equals 200. etc.
+$output = $num3;
+
+// Built-In PHP Functions
+// rand();
+$output = rand(); // random number between 0 and 2147483647
+$output = getrandmax(); // this will always equal 2147483647 as it's the maximum number for the rand() function.
+$output = rand(1, 10); // random number selected between the rand specified.
+
+// Rounding
+$output = round(4.2); // basic rounding
+$output = ceil(4.2); // rounds to the ceiling
+$output = floor(4.9); // rounds to the floor
+
+// Square Root
+$output = sqrt(64);
+
+// Pi
+$output = pi();
+
+// Absolute Value
+$output = abs(-4.6);
+
+// Max()
+$output = max(1, 2, 3, 4, 50);
+$output = max([1, 2, 3, 4, 50]);
+
+// Min()
+$output = min(1, 2, 3, 4, 50);
+$output = min([1, 2, 3, 4, 50]);
+
+// Formatting
+$output = number_format(1234567.191234, 2, '.', ','); // returns 1,234,567.19
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,7 +78,7 @@
   </header>
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
-      <!-- Output -->
+      <?= $output ?>
     </div>
   </div>
 </body>
