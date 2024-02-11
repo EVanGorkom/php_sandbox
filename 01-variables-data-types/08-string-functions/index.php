62e531e80dc16a430f0716d75e0c8bdd6e709d18
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-11 02:00:23 -0600

    Finish the most common string functions lesson

--- 01-variables-data-types/08-string-functions/index.php
+++ 01-variables-data-types/08-string-functions/index.php
@@ -1,5 +1,43 @@
 <?php
 $output = null;
+$string = 'Hello World';
+
+// strlen
+$output = strlen($string);
+
+// str_word_count
+$output = str_word_count($string);
+
+// strpos
+// // here we're giving it a string and looking for the index of where the substring starts
+$output = strpos($string, 'World');
+
+// Get specific char by index
+$output = $string[4];
+
+// substr
+// // here is kind of like the opposite of strpos. We're taking an index position and returning the number of character we want after that position (inclusive of starting point)
+$output = substr($string, 6, 5);
+
+// str_replace
+$output = str_replace('World', 'Universe', $string);
+// // this one replaces a substring with a new substring. Arg1 is the original, Arg2 is the new, and Arg3 is the string it all exists in.
+
+// strtolower
+// // lowercase everything
+$output = strtolower($string);
+
+// strtoupper
+// // capitalize everything
+$output = strtoupper($string);
+
+// ucwords 
+// // capitalize first letter of each word
+$output = ucwords($string);
+
+// trim
+// // removes the extra spaces
+$output = trim('     Hello World      ')
 ?>
 
 <!DOCTYPE html>
