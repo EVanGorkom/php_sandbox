1ed58834441bd61e90cdc2e1b2778372f2200de5
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-11 02:19:29 -0600

    Finish the date and time lesson

--- 01-variables-data-types/09-working-with-dates/index.php
+++ 01-variables-data-types/09-working-with-dates/index.php
@@ -1,5 +1,45 @@
 <?php
 $output = null;
+
+/*
+'Y' - the year
+'m' - the month
+'d' - the day
+'D' - the day of the week short name
+'l' - the full day of the week name
+'h' - the hour
+'i' - the minute
+'s' - the second
+'a' - AM/PM
+*/
+
+$output = date('Y');
+
+// used below is a 'time stamp' specific code for a specific time
+$output = date('Y', 936345600);
+
+// get timestamp from strtotime function
+$output = date('Y', strtotime('1999-09-01'));
+
+// get month
+$output = date('m');
+
+// get day
+$output = date('D');
+
+// All year, month, and day
+$output = date('Y-m-d');
+
+// hour
+$output = date('h');
+// minute
+$output = date('i');
+// second
+$output = date('s');
+// am/pm
+$output = date('a');
+// Put it all together
+$output = date('Y-m-d h:i:s a')
 ?>
 
 <!DOCTYPE html>
