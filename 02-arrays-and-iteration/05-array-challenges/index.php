58bba03531faef4292cda40d1162636c44d56aff
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-24 04:13:20 -0600

    Finish the array challenges section

--- 02-arrays-and-iteration/05-array-challenges/index.php
+++ 02-arrays-and-iteration/05-array-challenges/index.php
@@ -8,7 +8,12 @@
   4. Get the amount of numbers in the array and put into a variable.
   5. Print out 'The sum of the {amount} numbers is: {sum} '. For example, if the array is [1, 2, 3, 4, 5], the output should be 'The sum of the 5 numbers is: 15'. 
 */
+$numbers = [1, 2, 3, 4, 5];
+
+$counted = count($numbers);
+$total = $numbers[0] + $numbers[1] + $numbers[2] + $numbers[3] + $numbers[4];
 echo '<h3>Sum Of An Array</h3>';
+echo "The sum of the $counted numbers is $total";
 
 /*
   Challenge 2: Colors array
@@ -20,10 +25,17 @@ echo '<h3>Sum Of An Array</h3>';
 
 You should end up with the following array: ['yellow', 'pink', 'blue', 'red', 'purple']
 */
-
+$colors = ['red', 'blue', 'green', 'yellow'];
+$reversed = array_reverse($colors);
+array_push($reversed, 'purple', 'orange');
+array_replace($reversed, array(1 => 'pink'));
+array_pop($reversed);
+// var_dump($reversed);
 echo '<h3>Colors Array</h3>';
 
-$colors = ['red', 'blue', 'green', 'yellow'];
+echo '<pre>';
+print_r($colors);
+echo '</pre>';
 
 /*
   Challenge 3: Job listings array
@@ -35,3 +47,45 @@ $colors = ['red', 'blue', 'green', 'yellow'];
 */
 
 echo '<h3>Job Listings</h3>';
+
+$listings = [
+  [
+    'id' => 1,
+    'job_title' => 'Backend Developer',
+    'company' => 'Company Name',
+    'contact_email' => 'dude@email.com',
+    'contact_phone' => '123-456-7890',
+    'skills' => ['PHP', 'Python', 'Ruby', 'JavaScript', 'HTML', 'CSS']
+  ],
+  [
+    'id' => 2,
+    'job_title' => 'Full Stack Engineer',
+    'company' => 'XYZ Company',
+    'contact_email' => 'dude@email.com',
+    'contact_phone' => '123-456-7890',
+    'skills' => ['Typescript', 'React', 'Express', 'MongoDB']
+  ],
+  [
+    'id' => 3,
+    'job_title' => 'Artist',
+    'company' => 'Company 3',
+    'contact_email' => 'davinci@email.com',
+    'contact_phone' => '123-456-7890',
+    'skills' => ['Oils', 'Inks', 'Paints', 'Marble']
+  ]
+];
+
+array_push($listings, [
+  'id' => 4,
+  'job_title' => 'C# Developer',
+  'company' => '123 Company',
+  'contact_email' => 'kristen@email.com',
+  'contact_phone' => '123-456-7890',
+  'skills' => ['C#', 'Angular', 'JavaScript', 'HTML', 'CSS']
+]);
+
+echo $listings[1]['job_title'];
+
+echo '<br>';
+
+echo $listings[2]['skills'][0];
