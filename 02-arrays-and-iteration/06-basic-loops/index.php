771aec948c871ec6293fd8cf10c755a89c147ea9
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-28 16:29:45 -0600

    Finish the basic-loops lesson for php

--- 02-arrays-and-iteration/06-basic-loops/index.php
+++ 02-arrays-and-iteration/06-basic-loops/index.php
@@ -1,3 +1,43 @@
+<?php
+// for loop syntax
+// for (initialization; condition; update) {
+// code here
+// }
+
+// for ($i = 0; $i <= 10; $i++) {
+//     echo $i . '<br />';
+// }
+
+// while loop syntax
+// initialization;
+// while(condition) {
+    // code to do
+    // update
+// }
+
+// $i = 0;
+// while ($i <= 10) {
+//     echo $i . '<br />';
+//     $i++;
+// }
+
+// do-while loop syntax
+// initialization
+// do {
+    // code
+    // update
+// } while (condition);
+
+// NOTE: A do-while loop will always run the code at least once, THEN check the condition to see if it needs to run again, whereas the other loops check the condition first and then see if they need to run the loop.
+
+// $i = 0;
+// do {
+//     echo $i . '<br />';
+//     $i++;
+// } while ($i < 10)
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,6 +57,23 @@
     <div class="container mx-auto p-4 mt-4">
         <div class="bg-white rounded-lg shadow-md p-6 mt-6">
             <!-- Output -->
+            <ul>
+                <!-- for loop example -->
+                <!-- <?php for ($i = 0; $i <= 10; $i++) : ?>
+                    <li>Number: <?= $i ?></li>
+                    <?php endfor; ?> -->
+
+                <!-- while loop example -->
+                <!-- <?php $i = 0; while ($i <= 10): ?>
+                    <li>Number: <?= $i ?></li>
+                <?php $i++; endwhile; ?> -->
+                
+                <!--  -->
+                <?php $i = 0; do { ?>
+                    <li>Number: <?= $i ?></li>
+                <?php $i++; 
+                } while($i < 10) ?>
+            </ul>
         </div>
     </div>
 </body>
