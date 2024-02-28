806bd6a727c8f36aec281fb6b5df83364322cd6a
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-28 17:02:38 -0600

    Finish the lesson on iterating through arrays and hashes

--- 02-arrays-and-iteration/08-looping-through-arrays/index.php
+++ 02-arrays-and-iteration/08-looping-through-arrays/index.php
@@ -29,15 +29,39 @@ $users = [
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
       <!-- Output -->
       <h3 class="text-xl font-semibold mb-4">Using a for loop</h3>
-      <ul class="mb-6"></ul>
+      <ul class="mb-6">
+        <?php for ($i = 0; $i < count($names); $i++): ?>
+          <li>Name: <?= $names[$i]; ?></li>
+        <?php endfor ?>
+      </ul>
       <h3 class="text-xl font-semibold mb-4">Using a foreach loop</h3>
-      <ul class="mb-6"></ul>
+      <ul class="mb-6">
+        <?php foreach($names as $name) : ?>
+          <li>Name: <?= $name ?></li>
+        <?php endforeach; ?>
+      </ul>
       <h3 class="text-xl font-semibold mb-4">Using a foreach loop with index</h3>
-      <ul class="mb-6"></ul>
+      <ul class="mb-6">
+        <!-- this will print the index of the array as we loop through it -->
+        <?php foreach($names as $index => $name) : ?>
+          <li><?= $index . ': ' . $name ?></li>
+        <?php endforeach; ?>
+      </ul>
       <h3 class="text-xl font-semibold mb-4">Using a foreach loop with associative array</h3>
-      <ul class="mb-6"></ul>
+      <ul class="mb-6">
+        <?php foreach($users as $user): ?>
+          <li><?='Name: ' . $user['name'] . ' | Email: ' . $user['email'] ?></li>
+        <?php endforeach ?>
+      </ul>
       <h3 class="text-xl font-semibold mb-4">Getting key names and values from associative array</h3>
-      <ul class="mb-6"></ul>
+      <ul class="mb-6">
+        <!-- this syntax CAN pull the keys and their values by doing a nested iteration, but you typically don't do that -->
+        <?php foreach($users as $user): ?>
+          <?php foreach($user as $key => $value): ?>
+            <li><?= $key . ': ' . $value ?></li>
+          <?php endforeach ?>
+        <?php endforeach ?>
+      </ul>
     </div>
   </div>
 </body>
