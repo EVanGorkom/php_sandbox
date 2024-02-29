a3d35e4256de7be0e40e751a0459d668fdee04b0
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-28 19:42:44 -0600

    Finish the first two lessons of conditionals in php

--- 03-control-structures/01-if-statements/index.php
+++ 03-control-structures/01-if-statements/index.php
@@ -1,10 +1,38 @@
 <?php
-
+$age = 30;
 // If statement
+// if (condition) {
+  // Code to be executed if the condition is true
+// }
+if ($age >= 21) {
+  echo 'You are old enough to drink in the US';
+}
+
 
 // If-Else
+if ($age >= 21) {
+  echo 'You are old enough to drink in the US';
+} else {
+  echo 'You are NOT old enough to drink in the US';
+}
 
 // Nested if statement
+if ($age >= 21) {
+  echo 'You are old enough to drink in the US';
+} else {
+  if ($age >= 18) {
+    echo 'You are old enough to vote in the US';
+  } else {
+    echo 'You are NOT old enough to drink or vote in the US';
+  }
+}
 
 // If-Else-If
-
+if ($age >= 21) {
+  echo 'You are old enough to drink in the US';
+} else if ($age >= 18) {
+  echo 'You are old enough to vote in the US';
+} else {
+  echo 'You are NOT old enough to drink or vote in the US';
+}
+?>
