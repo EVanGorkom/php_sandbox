b33f97e7a50cd598cccf01750cded46c59270241
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-29 20:01:19 -0600

    Finish the null coalescing operator and names-challenge

---
+++ 03-control-structures/09-null-coalescing-operator/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// $favoriteColor = 'red';
+// $secondFavoriteColor = 'green';
+
+//// ternary operator
+// $color = isset($favoriteColor) ? $favoriteColor : 'blue';
+
+//// null coalescing operator
+// $color = $favoriteColor ?? 'blue';
+// if condition on left == null
+// then do the condition on the right
+
+// // ternary
+// $color = isset($favoriteColor) ? $favoriteColor : (isset($secondFavoriteColor) ? $secondFavoriteColor : 'blue');
+
+// // Null coalescing
+// $color = $favoriteColor ?? $secondFavoriteColor ?? "blue";
+
+echo $color;
