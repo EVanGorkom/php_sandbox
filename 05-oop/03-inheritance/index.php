7c048ba528fc8dce2dd01ecd03136cd6e84eeeb0
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-03-10 21:14:59 -0500

    Finish the inheritance and access lessons

--- 05-oop/03-inheritance/index.php
+++ 05-oop/03-inheritance/index.php
@@ -17,3 +17,36 @@ class User
     echo $this->name . ' logged in <br>';
   }
 }
+
+class Admin extends User 
+{
+  public $level;
+  
+  public function __construct($name, $email, $level)
+  {
+    $this->level = $level;
+    parent::__construct($name, $email);
+  }
+  
+  public function login()
+  {
+    echo 'Admin ' . $this->name . ' logged in';
+  }
+
+  public function getStatus()
+  {
+    echo $this->status;
+  }
+}
+
+$admin1 = new Admin('Tom Smith', 'tom@gmail.com', 5);
+echo $admin1->name;
+echo '<br>';
+echo $admin1->email;
+echo '<br>';
+$admin1->login();
+echo '<br>';
+$admin1->getStatus();
+echo '<br>';
+
+var_dump($admin1);
